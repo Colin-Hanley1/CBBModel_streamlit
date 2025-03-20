34a5d822ced8c0a62e44d407f0877ab018bd4c90
Colin Hanley <77418750+Colin-Hanley1@users.noreply.github.com>
2025-03-20 08:08:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -292,7 +292,7 @@ def main():
     st.set_page_config(page_title="College Basketball Prediction Model", layout="wide")
     
     st.title("College Basketball Prediction Model")
-    st.write("Last updated 3/19/25 at 11:39")
+    st.write("Last updated 3/20/25 at 08:08")
     
     # Load model and data
     model, scaler, df, teamsheet, explainer, feature_names = load_model_and_data()
